12391e355156f99be9e3e8b59c6c8460f9d87d08
Guilheme Augusto Vieira <guilhermeavdino@gmail.com>
2021-12-01 02:05:23 -0300

    Update InjecaoDependencia.ts

--- src/API/Configuracoes/InjecaoDependencia.ts
+++ src/API/Configuracoes/InjecaoDependencia.ts
@@ -9,12 +9,19 @@ import { ConexaoMongo } from "../../Infrastrutura/BaseDeDados/ConexaoMongo";
 import { IServicoEmprestimo } from "../../Aplicacao/Interfaces/IServicoEmprestimo";
 import { ServicoEmprestimo } from "../../Aplicacao/Servicos/ServicoEmprestimo";
 
+import { IServicoParcela } from "../../Aplicacao/Interfaces/IServicoParcela";
+import { ServicoParcela } from "../../Aplicacao/Servicos/ServicoParcela";
+
 container.register<IServicoUsuario>("ServicoUsuario", 
   { useClass: ServicoUsuario },
   { lifecycle: Lifecycle.ResolutionScoped });
 
   container.register<IServicoEmprestimo>("ServicoEmprestimo", 
   { useClass: ServicoEmprestimo },
+  { lifecycle: Lifecycle.ResolutionScoped });
+
+  container.register<IServicoParcela>("ServicoParcela", 
+  { useClass: ServicoParcela },
   { lifecycle: Lifecycle.ResolutionScoped });
 
 container.register<IConexaoMongo>("ConexaoMongo", 
